3c92ab5f97f5b22ebca4bfa8242d2eeae01b7753
Raccoon <a85068278@163.com>
2018-10-19 10:17:48 +0800

    提示问题 更改

--- src/views/Navigation2.vue
+++ src/views/Navigation2.vue
@@ -116,7 +116,7 @@
           <div class="modal-content bor-rad">
             <div class="modal-header">
               <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
-                &times 
+
               </button>
               <h4 class="modal-title" id="myModalLabel" v-if="isEdit==0">
                 添加{{pageLoction}}

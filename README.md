168f7ff81349d9bea82de3fa8efe4eaa9a326034
Raccoon <a85068278@163.com>
2018-10-22 18:18:08 +0800

    update

--- README.md
+++ README.md
@@ -8,8 +8,8 @@
 # install dependencies
 npm install
 
-# serve with hot reload at localhost:8080
-npm run dev
+# serve with hot reload at localhost:8080v
+npm run devv
 
 # build for production with minification
 npm run build
@@ -25,4 +25,5 @@ npm test
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
 test1235
--- src/views/admin/Composition.vue
+++ src/views/admin/Composition.vue
@@ -10,19 +10,20 @@
             <div class="content" > 
                 <div class="content-top">
                     <ul style="padding-top:15px;font-size:12px;" id="compsearch" >
-                        <li class="compsearch" style="width:530px;padding:0 160px 0  100px;margin-left:30px;" v-for="item in list" :key="item.id" :id="item.ordernum">
+                        <li class="compsearch" style="width:530px;padding:0 160px 0  100px;margin-left:30px;" v-for="item in list" :key="item.id" :id="item.id">
                             <div style="position:relative" >
                                 <img :src="'https://www.entertry.com/images/img/'+item.imgpath" alt="">
                                 <span style="position:absolute;left:-70px;top:3px;">{{item.name}}</span>
                                 <span style="position:absolute;right:-100px;top:3px;">是否显示</span>
                                 <label class ="switch switch-mini" style="position:absolute;right:-120px;top:0px;">
-                                    <input type ="checkbox" :value="item.isvisited" :name="item.name" :checked="item.isvisited"/>
+                                    <input type ="checkbox" :id="item.id" :value="item.id" v-model="isvisited">
                                 </label>
                             </div>
                         </li>
                     </ul> 
                     <div class="text-left" style="padding:10px 0 0 130px;">
                         <button style="background-color:#45abf5;color:white;border:none;height:25px;width:40px;" @click="listindexchange()">保存</button>
+                        <button style="height:25px;" @click="init()">初始化</button>
                     </div>      
                 </div>
             </div>
@@ -39,7 +40,9 @@ import { mapState } from 'vuex';
 export default {
     data() {
         return {
-            list: []
+            list: [],
+            tmplist: [],
+            isvisited:[]
         }
     },
     computed: {
@@ -66,26 +69,53 @@ export default {
     methods: {
         listindexchange(){
             let serial = $("#compsearch").sortable('toArray');
-            let serial1 = $("#compsearch").sortable('serialize');
-            console.log(serial1);
-            this.$http.post("api/main.php?ywtype=11202"+"&tb="+this.table,serial).then(response=>{
-                console.log(response);
-            })
+            this.httpreq("11202",serial);
+        },
+        init(){
+            if(confirm("确定初始化？")){
+                this.httpreq("11203");
+            }
+        },
+        httpreq(ywtype){
+            switch(ywtype){
+                case "11201":  //获取数据库列表
+                    this.$http.get("api/main.php?ywtype="+ywtype+"&tb="+this.table).then(response=>{
+                        this.list = response.data;
+                        for(var item of this.list){
+                            if(item.isvisited=="1"){
+                                this.isvisited.push(item.id);
+                            }
+                        }
+                        console.log(this.isvisited)
+                    });
+                    break;
+                case "11202":   //提交
+                    this.$http.post("api/main.php?ywtype="+ywtype+"&tb="+this.table,arguments[1]).then(response=>{})
+                    break;
+                case "11203":   //初始化
+                    let that = this 
+                    that.$http.post("api/main.php?ywtype="+ywtype+"&tb="+that.table).then(response=>{
+                        that.httpreq("11201");
+                    })
+                    break;
+                default:
+                    break;
+            }
         }
+
     },
     /* event listeners code in mounted function*/
     mounted: function(){
         var comp = $("#compsearch")
         comp.sortable( {
-            update:function(){
-                let serial = comp.sortable('toArray');
-            }
+            // update:function(){
+            //     let serial1 = $("#compsearch").sortable('serialize',{key: "orderid"});
+            //     console.log(serial1);
+            // }
         });
         $("#compsearch").disableSelection();
 
-        this.$http.get("api/main.php?ywtype="+"11201"+"&tb="+this.table).then(response=>{
-            this.list = response.data
-        })
+        this.httpreq("11201");
 
     },
     destroyed: function(){}
